329ea38ae9a337b0fc38f74671a2db41412677ea
LuisAlvarez98 <luisfalvarezsanchez@gmail.com>
2020-11-27 19:11:10 -0600

    Added image to 404 error

--- FE/app/src/views/ErrorComponent/ErrorComponent.tsx
+++ FE/app/src/views/ErrorComponent/ErrorComponent.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-
+import Error404 from "../../img/error404.png";
 const Container = styled.div`
   align-items: center;
   justify-content: center;
   display: flex;
   color: white;
-  height: 750px;
+  height: 100vh;
   width: 100%;
 `;
 
 const ErrorComponent = () => {
   return (
     <Container>
-      <p>Error 404</p>
+      <img width="100%" height="20%" src={Error404} />
     </Container>
   );
 };
